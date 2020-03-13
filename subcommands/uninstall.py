6eeea1f63d2aa44a9520e05c247708795a7d98f7
Thomas Draebing <thomas.draebing@sap.com>
2020-03-13 09:39:41 +0100

    Rewrite the scripts in python
    The scripts were written in bash. Using bash became quite unwieldy.
    
    Python by nature can deal well with yaml and is thus better suited
    in dealing with the yaml-based configuration files. This change
    rewrites the original scripts staying as close as possible to the
    original ones.
    
    Right now, the python scripts call subprocesses a lot to work with
    the tools, which were already used before. At least for yaml-
    templating there may be better tools that have a python integration,
    which could be used in the future.
    
    Change-Id: Ida16318445a05dcfdada9c7a56a391e4827f02e7

---
+++ subcommands/uninstall.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+from ._globals import HELM_CHARTS
+
+
+def _get_yn_response(message):
+    while True:
+        response = input(message)
+        if response == "y":
+            return True
+
+        if response == "n":
+            return False
+
+        print("Unknown input.")
+
+
+def _remove_helm_deployment(chart, namespace):
+    deployment_name = f"{chart}-{namespace}"
+    if _get_yn_response(
+        f"This will remove the deployment {deployment_name}. Continue (y/n)? "
+    ):
+        command = ["helm", "uninstall", deployment_name, "-n", namespace]
+        subprocess.check_output(command)
+
+
+def _delete_namespace(namespace):
+    if _get_yn_response(
+        f"This will remove the namespace {namespace}. Continue (y/n)? "
+    ):
+        command = ["kubectl", "delete", "ns", namespace]
+        subprocess.check_output(command)
+
+
+def uninstall(config_manager):
+    """Uninstall the monitoring setup.
+
+    Arguments:
+        config_manager {AbstractConfigManager} -- ConfigManager that contains the
+          configuration of the monitoring setup to be uninstalled.
+    """
+    namespace = config_manager.get_config()["namespace"]
+    for chart in HELM_CHARTS:
+        _remove_helm_deployment(chart, namespace)
+    _delete_namespace(namespace)
